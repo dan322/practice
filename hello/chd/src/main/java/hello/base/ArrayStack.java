228aa09c6b4aa44fbcee8fbb405c45fdeff1c742
chendan5 <chendan5@lenovo.com>
2018-10-10 19:04:23 +0800

    simpleLinked and array stack

--- hello/chd/src/main/java/hello/base/ArrayStack.java
+++ hello/chd/src/main/java/hello/base/ArrayStack.java
@@ -2,4 +2,9 @@ package hello.base;
 
 public class ArrayStack {
     // todo
+    private int length;
+    private int tailIn;
+    private int headIn;
+
+
 }
--- hello/chd/src/main/java/hello/base/simpleLink/SimpleLinked.java
+++ hello/chd/src/main/java/hello/base/simpleLink/SimpleLinked.java
@@ -6,32 +6,12 @@ public class SimpleLinked {
     // TODO
     private Node head = null;
 
-    public void addToHead (int value)
-    {
-        Node node = new Node(value);
-        if (head != null || head.next != null)
-            node.next = head.next;
-        head = node;
-    }
-
-    public void addToNodeAfter (Node node, int value)
-    {
-        Node newNode = new Node(value);
-        newNode.next = node.next;
-        node.next = newNode;
-    }
-
-    public void addToNodeBefore(Node node, int value)
-    {
-
-    }
-
     public void deleteNode(Node node)
     {
-        if (head == null) return;
-        Node searchNode = head;
+        if (head.next == null) return;
+        Node searchNode = head.next;
         Node preNode = null;
-        while (searchNode.data != node.data)
+        while (searchNode.next != node.next)
         {
             preNode = searchNode;
             if (searchNode.next == null)
@@ -39,7 +19,7 @@ public class SimpleLinked {
             searchNode = searchNode.next;
         }
         if (preNode == null) {
-            head = null;
+            head.next = null;
             return;
         }
         preNode.next = searchNode.next;
@@ -48,7 +28,85 @@ public class SimpleLinked {
     public void insertNode(Node node, int value)
     {
         Node newNode = new Node(value);
-        
+        if (head.next == null) {
+            head.next = newNode;
+            return;
+        }
+        Node searchNode = head.next;
+        while (searchNode.next == node.next) {
+            node.next = searchNode.next;
+            searchNode.next = node;
+            break;
+        }
     }
 
+    public void deleteNodeAfter(Node node)
+    {
+        if (head.next == null ) return;
+        Node searchNode = head.next;
+        while ( searchNode.next == node.next)
+        {
+            node.next = null;
+            break;
+        }
+    }
+
+    public void deleteTailN(int n)
+    {
+
+    }
+
+    public void reverseLink ()
+    {
+        if (head.next == null) return;
+        Node nextNode = head.next;
+        Node secondNode = nextNode.next;
+        while (secondNode != null)
+        {
+            nextNode.next = secondNode.next;
+            head.next = secondNode;
+            secondNode.next = nextNode;
+            if (nextNode.next == null)
+                return;
+            secondNode = nextNode.next;
+        }
+    }
+
+    // todo linked examine circle
+    public boolean isCircle()
+    {
+
+        return false;
+    }
+
+    /**
+     *  merge two linked
+     * @param l1
+     * @return
+     */
+    public SimpleLinked mergeLinked(SimpleLinked l1)
+    {
+        Node searchNode = head.next;
+        if (searchNode == null) {
+            return l1;
+        }
+        while (searchNode.next != null) {
+            searchNode = searchNode.next;
+        }
+        searchNode.next = l1.head.next;
+        l1.head.next = searchNode;
+        return l1;
+    }
+
+    public Node findNode(int data)
+    {
+        if (head.next == null) return null;
+        Node searchNode = head.next;
+        while (searchNode.data != data) {
+            searchNode = searchNode.next;
+            if (searchNode == null )
+                return null;
+        }
+        return searchNode;
+    }
 }
